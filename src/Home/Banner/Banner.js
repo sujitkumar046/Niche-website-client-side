6574ddab20b05cb7593c785cd1876ce59cdadd36
sujitkumar046 <sujitmodak046@gmail.comm>
2021-11-18 22:55:01 +0600

    submit

--- src/Home/Banner/Banner.js
+++ src/Home/Banner/Banner.js
@@ -6,8 +6,8 @@ const Banner = () => {
         <div className='container-fluid banner-container'>
         
             <div className='col-md-6 col-lg-6 sm-col-12 p-2 ms-auto'>
-            <h3 className='text-warning text-start mt-5'>Coffee Factory</h3> <br />
-            <h1 className='text-white text-start fs-1 fw-bold'>Enjoy Delicios Coffee</h1>
+            <h3 className='text-warning text-start mt-5'>We Sell Best Coffee Machines!</h3> <br />
+            <h1 className='text-white text-start fs-1 fw-bold'>Enjoy Delicious Coffee</h1>
             <br />
             <h4 className='text-start text-light '>Test heavenly flavour of coffee with the best coffe maker machines in the world! We offer best cappacino, espresso machines and deliver all over the wworld.
         <br />
